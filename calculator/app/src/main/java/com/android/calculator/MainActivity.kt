28899f749fc63ecfb90f0b4d42f31cacfaa85a68
vonhuvinh149 <vonhuvinh149@gmail.com>
2023-06-28 18:45:57 +0700

    calculator

--- calculator/app/src/main/java/com/android/calculator/MainActivity.kt
+++ calculator/app/src/main/java/com/android/calculator/MainActivity.kt
@@ -10,11 +10,15 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var number1: EditText;
     private lateinit var number2: EditText;
-    private lateinit var operatorUp: Button;
-    private lateinit var operatorDown: Button;
-    private lateinit var textView: TextView ;
+    private lateinit var addition: Button;
+    private lateinit var subtraction: Button;
+    private lateinit var multiplication: Button
+    private lateinit var division: Button
+    private lateinit var textView: TextView;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +26,55 @@ class MainActivity : AppCompatActivity() {
 
         number1 = findViewById(R.id.nb1);
         number2 = findViewById(R.id.nb2);
-        operatorUp = findViewById(R.id.button)
-        operatorDown = findViewById(R.id.button2)
-        textView= findViewById(R.id.textView);
+        addition = findViewById(R.id.button)
+        subtraction = findViewById(R.id.button2)
+        multiplication = findViewById(R.id.button3)
+        division = findViewById(R.id.button4)
+        textView = findViewById(R.id.textView);
         var result: Int;
 
-        operatorUp.setOnClickListener{
-            if (number1.length() > 0  && number2.length() > 0){
-                result = number1.getText().toString().toInt() + number2.getText().toString().toInt()
-                textView.text = result.toString();
-            }else{
-                Toast.makeText(this@MainActivity , "vui long nhap" , Toast.LENGTH_SHORT).show()
+        addition.setOnClickListener {
+            val nb1 = number1.text.toString()
+            val nb2 = number2.text.toString()
+            if (nb1.isEmpty() || nb2.isEmpty()) {
+                Toast.makeText(this@MainActivity, "vui long nhap", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+            result = nb1.toInt() + nb2.toInt()
+            textView.text = result.toString()
+        }
+
+        subtraction.setOnClickListener {
+            val nb1 = number1.text.toString()
+            val nb2 = number2.text.toString()
+            if (nb1.isEmpty() || nb2.isEmpty()) {
+                Toast.makeText(this@MainActivity, "vui long nhap", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            result = nb1.toInt() - nb2.toInt()
+            textView.text = result.toString()
+        }
+
+        multiplication.setOnClickListener {
+            val nb1 = number1.text.toString()
+            val nb2 = number2.text.toString()
+            if (nb1.isEmpty() || nb2.isEmpty()) {
+                Toast.makeText(this@MainActivity, "vui long nhap", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            result = nb1.toInt() * nb2.toInt()
+            textView.text = result.toString()
+        }
+
+        division.setOnClickListener {
+            val nb1 = number1.text.toString()
+            val nb2 = number2.text.toString()
+            if (nb1.isEmpty() || nb2.isEmpty()) {
+                Toast.makeText(this@MainActivity, "vui long nhap", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            result = nb1.toInt() / nb2.toInt()
+            textView.text = result.toString()
         }
     }
 }
